158eed01099c72e73bf126eda513280424416094
Tom Short <tom.a.short@gmail.com>
2018-12-01 19:24:08 +0000

    Add show trailer

--- entertainment_centre.py
+++ entertainment_centre.py
@@ -10,3 +10,8 @@ avatar = Movie("Avatar",
                "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 print(f"{avatar.title}: {avatar.storyline}")
+withnail_and_i = Movie("Withnail & I",
+                       "Two unemployed actors go on holiday by mistake.",
+                       "https://upload.wikimedia.org/wikipedia/en/f/fe/Withnail_and_i_poster.jpg",
+                       "https://www.youtube.com/watch?v=d9Z0DV33gAY")
+print(f"{withnail_and_i.title}: {withnail_and_i.storyline}")
--- movie.py
+++ movie.py
@@ -1,3 +1,5 @@
+import webbrowser
+
 class Movie():
 
     def __init__(self, movie_title, movie_story, movie_poster_url, movie_trailer_url):
@@ -5,3 +7,6 @@ class Movie():
         self.storyline = movie_story
         self.poster_url = movie_poster_url
         self.trailer_url = movie_trailer_url
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_url)
